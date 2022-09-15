24dd27f2b8c8dcb7891ad26080294c30dfd0cab2
TomDal <lennox77777@gmail.com>
2022-09-15 20:29:20 +0200

    pom fix and upgrade, second test method added

--- src/test/java/testing/AccountTest.java
+++ src/test/java/testing/AccountTest.java
@@ -3,14 +3,25 @@ package testing;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class AccountTest {
     @Test
     void myTest(){
-    Account newAccount = new Account();
-        assertFalse(newAccount.isActive());
-
-
-
+        Account newAccount = new Account();
+        assertFalse(newAccount.isActive(), "Check if new account is not active");
     }
+
+    @Test
+    void myTest2(){
+        Account newAccount = new Account();
+        assertFalse(newAccount.isActive());
+        newAccount.activate();
+        assertTrue(newAccount.isActive());
+    }
+
+
+
+
+
 }
