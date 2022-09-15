50e85823327e51ceec1fe2793313a4861badce86
TomDal <lennox77777@gmail.com>
2022-09-15 18:14:36 +0200

    JUnit dependencies upgrade, first test class added

---
+++ src/test/java/testing/AccountTest.java
@@ -0,0 +1,16 @@
+package testing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+class AccountTest {
+    @Test
+    void myTest(){
+    Account newAccount = new Account();
+        assertFalse(newAccount.isActive());
+
+
+
+    }
+}
